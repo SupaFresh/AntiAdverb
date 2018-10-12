98f497f510f1cf35329a60b6e2c513dbe701d69b
SupaFresh <221895@c1mpd493g943.isd720.com>
2018-10-12 09:57:26 -0500

    add

---
+++ Run.py
@@ -0,0 +1,7 @@
+import easygui
+import list.py
+
+reply = easygui.enterbox(msg = 'Enter a word', title = 'Anti Adverb', strip = True,)
+if reply = i in alist:
+    easygui.msgbox(reply+" is a adverb!")
+else:
